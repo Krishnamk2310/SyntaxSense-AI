eac095c2f930cbd76bb3773cadf445aee4ec55ce
Markandey Krishna Mishra <143481619+Krishnamk2310@users.noreply.github.com>
2025-07-23 18:04:55 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const aiRoutes = require('./routes/ai.routes.js')
 const cors = require('cors')
+const aiRoutes = require('./routes/airoutes');
 
 const app = express()
 app.use(cors())
-
 app.use(express.json())
+app.use('/ai', aiRoutes); 
 
 app.get('/',(req,res)=>{
     res.send('Hello world')
