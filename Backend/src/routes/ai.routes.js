5331f99b3fe46010309cc8bf5d512fbf4ce04e0f
Markandey Krishna Mishra <143481619+Krishnamk2310@users.noreply.github.com>
2025-07-23 17:47:26 +0530

    Update ai.routes.js

--- Backend/src/routes/ai.routes.js
+++ Backend/src/routes/ai.routes.js
@@ -2,6 +2,6 @@ const express = require('express')
 const router = express.Router();
 const aiController = require('../controllers/ai.controller.js')
 
-router.post("/get-review",aiController.getReview)
+router.post("https://syntaxsense-ai-frontend.onrender.com/",aiController.getReview)
 
 module.exports = router;
